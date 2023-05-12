c5102dc4ae1ffac5e5c6bf30473d02ab57387681
Eliot Gronstal <eliot.lauren@gmail.com>
2023-05-12 16:08:13 -0700

    update Api Route and remove [controller]

--- GratShiftSaveApi/Controllers/UserController.cs
+++ GratShiftSaveApi/Controllers/UserController.cs
@@ -10,7 +10,7 @@ using System.Text;
 namespace GratShiftSaveApiController.Controllers
 {
   [ApiController]
-  [Route("api/[controller]")]
+  [Route("api/")]
   public class UserController : ControllerBase
   {
     private readonly UserManager<IdentityUser> _userManager;
